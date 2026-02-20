b05fa7ca1d4a445d5ce0fdfbabaea190c9e4a7d4
carlossantos-lang <carlossantos@spun.com.br>
2026-02-20 20:24:17 -0300

    Update MGBREMP.js

--- MGBREMP.js
+++ MGBREMP.js
@@ -8,7 +8,7 @@ puppeteer.use(StealthPlugin());
 // ================= DISCORD =================
 const DISCORD_CONFIG = {
   webhookUrl: process.env.DISCORD_WEBHOOK_URL_MGBREMP,
-  threadId: process.env.DISCORD_THREAD_IDMG
+  threadId: process.env.DISCORD_THREAD_ID_MGBREMP
 };
 
 // ================= URLS =================
