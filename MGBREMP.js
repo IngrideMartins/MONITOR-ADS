d2f4006ed5a6c5995efea82cb299ae6dc09bb9f7
carlossantos-lang <carlossantos@spun.com.br>
2026-02-20 20:30:58 -0300

    Update MGBREMP.js

--- MGBREMP.js
+++ MGBREMP.js
@@ -14,9 +14,7 @@ const DISCORD_CONFIG = {
 // ================= URLS =================
 const urls = [
 'https://emprestimo.altarendabr.com/pt-br-recomendacao-de-emprestimos-4',
-'https://emprestimo.sofinancas.com/p1-emprestimos-recomendados-r',
-'https://br.creativepulse23.com/convite-para-o-cartao-casas-bahia'
-];
+'https://emprestimo.sofinancas.com/p1-emprestimos-recomendados-r'];
 
 // ================= TARGET GROUPS (REGRA: OU em TODOS os grupos) =================
 const TARGET_GROUPS = {
