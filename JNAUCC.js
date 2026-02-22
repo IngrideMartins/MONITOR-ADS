4df53095487fab0b635b9abeb266727be0b91746
Ingride Martins <169106976+IngrideMartins@users.noreply.github.com>
2026-02-22 20:35:39 -0300

    Remove 'rewarded' from TARGET_GROUPS and scroll selector

--- JNAUCC.js
+++ JNAUCC.js
@@ -19,8 +19,7 @@ const urls = [
 // ================= TARGET GROUPS (REGRA: OU em TODOS os grupos) =================
 const TARGET_GROUPS = {
   top: ['mob_top', 'desk_top'],
-  interstitial: ['interstitial'],
-  rewarded: ['rewarded','offerwall']
+  interstitial: ['interstitial']
 };
 
 // ================= CONFIGS =================
@@ -546,7 +545,7 @@ async function processarUrl(url, browser) {
 
       await scrollAteSelector(
         page,
-        '#mob_top, #desk_top, [id*="mob_top"], [id*="desk_top"], [id*="rewarded"], [id*="interstitial"]',
+        '#mob_top, #desk_top, [id*="mob_top"], [id*="desk_top"], [id*="interstitial"]',
         10
       ).catch(() => {});
 
