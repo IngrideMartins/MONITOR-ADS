62857aa79d733d32b997bba4bfb798452dde13cf
Ingride Martins <169106976+IngrideMartins@users.noreply.github.com>
2026-02-22 19:58:20 -0300

    Fix URL formatting in JNAUCC.js

--- JNAUCC.js
+++ JNAUCC.js
@@ -14,7 +14,7 @@ const DISCORD_CONFIG = {
 // ================= URLS =================
 const urls = [
 'https://au.zienic.com/discover-the-credit-card',
-'https://au.genialcredito.com/recommendation-card-2-r'
+'https://au.genialcredito.com/recommendation-card-2-r'];
 
 // ================= TARGET GROUPS (REGRA: OU em TODOS os grupos) =================
 const TARGET_GROUPS = {
