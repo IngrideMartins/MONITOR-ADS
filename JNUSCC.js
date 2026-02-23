be35fe78b4b5a79e2002f0fb5a08ba3c115043b9
Ingride Martins <169106976+IngrideMartins@users.noreply.github.com>
2026-02-22 22:20:47 -0300

    Update JNUSCC.js

--- JNUSCC.js
+++ JNUSCC.js
@@ -21,7 +21,7 @@ const urls = [
 
 // ================= TARGET GROUPS (REGRA: OU em TODOS os grupos) =================
 const TARGET_GROUPS = {
-  top: ['mob_top', 'desk_top' 'enrdm_top'],
+  top: ['mob_top', 'desk_top', 'enrdm_top'],
   interstitial: ['interstitial']
 };
 
