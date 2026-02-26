cdeaab93b34036d672512027641856575585e344
Ingride Martins <169106976+IngrideMartins@users.noreply.github.com>
2026-02-26 02:00:52 -0300

    Update MGBREMP.js

--- MGBREMP.js
+++ MGBREMP.js
@@ -13,8 +13,7 @@ const DISCORD_CONFIG = {
 
 // ================= URLS =================
 const urls = [
-'https://emprestimo.altarendabr.com/pt-br-recomendacao-de-emprestimos-4',
-'https://emprestimo.sofinancas.com/p1-emprestimos-recomendados-r'];
+'https://emprestimo.altarendabr.com/pt-br-recomendacao-de-emprestimos-4'];
 
 // ================= TARGET GROUPS (REGRA: OU em TODOS os grupos) =================
 const TARGET_GROUPS = {
